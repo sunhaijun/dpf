d59b2082dea1dd0eaa6504e4b210cf2d85af66d2
sunhaijun <sunhaijun@msn.com>
2024-08-06 23:30:46 +0800

    1

--- web/app/components/header/account-about/index.tsx
+++ web/app/components/header/account-about/index.tsx
@@ -39,7 +39,7 @@ export default function AccountAbout({
           <LogoSite className='mx-auto mb-2' />
           <div className='mb-3 text-center text-xs font-normal text-gray-500'>Version {langeniusVersionInfo?.current_version}</div>
           <div className='mb-4 text-center text-xs font-normal text-gray-700'>
-            <div>© {dayjs().year()} LangGenius, Inc., Contributors.</div>
+            <div>© {dayjs().year()} DPF.</div>
             <div className='text-[#1C64F2]'>
               {
                 IS_CE_EDITION
